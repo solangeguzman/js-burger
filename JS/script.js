b0cb7f8bda2f6b819e21ca2c11a8c40e410e5769
solangeguzman <solangeguzmanduchen@gmail.com>
2021-06-07 22:04:43 +0200

    codice da rivedere

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,22 @@
-var calcoloSum = document.getElementById('somma-totale');
-var resultSumElement = document.getElementById('result-sum')
+var totPrice = document.getElementById('calculate');
+var resultPrice = document.getElementById('result-sum')
 
-calcoloSum.addEventListener('click', function () {
+totPrice.addEventListener('click', function () {
 
-    var result = 70;
+    var result = 0;
 
-    var Num = document.getElementByIdClassName('somma');
-
-    for (var i = 0; i< Num.length; i++){
-
-       if (Num[i].checked){
-          
-          result += parseInt(Num[i].value)
+    var SelectionFood = document.getElementByIdClassName('Food');
+    var discount = document.getElementByIdClassName('sconti');
+    var discountPrice = 0;
+    for (var i = 0; i< selectionFood.length; i++){
+        if (selectionFood[i].checked){
+           result += parseInt(Num[i].value)
        
         }
 
     }
-    document.getElementById('result').innerHTML = 'Your price is:' + result;
+    if (discount.value ===''){
+        discountPrice= parseInt(result/100*10);
+    }
+    resultPrice.innerHTML = 'Your price is:' + (result-discountPrice);
 })
